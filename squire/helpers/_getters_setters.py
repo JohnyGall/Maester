d9c155e0879f1a8194e4422021e4386c34914fdc
John Gallagher <johny.gallagher@gmail.com>
2016-08-18 12:26:57 -0400

    outlined squire class and wrote getters_setters

---
+++ squire/helpers/_getters_setters.py
@@ -0,0 +1,17 @@
+def set_apk_path(self, path):
+    self._apk_path = path
+
+def set_aapt_path(self, path):
+    self._aapt_path = path
+
+def set_adb_path(self, path):
+    self.adb_path = path
+
+def get_adb_path(self):
+    return self._adb_path
+
+def get_aapt_path(self):
+    return self._aapt_path
+
+def get_adb_path(self):
+    return self._adb_path
---
+++ squire/squire.py
@@ -0,0 +1,13 @@
+import os
+import sys
+sys.path.insert(0, '_helpers')
+import _getters_setters
+import _init
+import _get_devices
+import _ui_elements_helper
+
+class squire:
+    _apk_path = ""
+    _appt_path = ""
+    _adb_path = ""
+    _ui_elements = {}
