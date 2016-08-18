2d5d2d13beaa765b99f374b3a4fb55d040a66ff8
Joe Campbell <jcamp1095@gmail.com>
2016-08-18 14:02:07 -0400

    restructure

---
+++ squire/helpers/_get_devices.py
@@ -0,0 +1,85 @@
+'''
+This module returns information about the connected phones such as the udids,
+model, and name
+'''
+
+import init
+import string
+import os
+
+adb_path = '~/Library/Android/sdk/platform-tools' 
+
+#returns a dictionary of relevant information about all
+#connected devices.  Each device will have information 
+#about its udid, model, and name returned
+def get_info():
+    devices = []
+
+    lines = set_up_list()
+
+    for i in range (0, len(lines)):
+        info = {"udid": get_phone(lines[i]), 
+                "model": get_model(lines[i]), 
+                "name": get_name(lines[i])}
+
+        devices.append(info)
+
+    return devices
+
+#get udid
+def get_phone(id_line):
+    device = ""
+    i = 0
+
+    while id_line[i] != ' ':
+        device += id_line[i]
+        i += 1
+
+    return device
+
+def get_model(id_line):
+    model = ""
+    i = find_colon(id_line, 2) + 1 #find 2nd colon from the right, go to the space right after
+
+    while id_line[i] != ' ':
+        model += id_line[i]
+        i += 1
+            
+    return model
+
+def get_name(id_line):
+    name = ""
+    i = find_colon(id_line, 1) + 1 #find 1st colon from the right, go to the space right after
+
+    while i != len(id_line) - 1:
+        name += id_line[i]
+        i += 1
+
+    return name
+
+
+def set_up_list():
+    curr_dir = os.getcwd()
+    os.chdir(os.path.expanduser(adb_path))
+    output = init.run_cmd_with_output("./adb devices -l")
+    os.chdir(curr_dir)
+    lines = output.splitlines() #make each line an element of the array lines
+    lines.pop(0) #get rid of the info line returned
+    return lines
+
+#find the indexed number colon from the right and return the index 
+def find_colon(line, index):
+    colons = list()
+    i = len(line) - 1
+
+    while i >= 0:
+        if line[i] == ':':
+            colons.append(line[i])
+        if len(colons) == index:
+            return i
+        i = i - 1
+
+
+
+
+
---
+++ squire/helpers/_ui_elements_helper.py
@@ -0,0 +1,98 @@
+'''
+The Xpaths and IDs of all the UI elements we refer to in unit tests.
+They are all here so when the UI changes we don't have to update a huge number of
+files
+'''
+
+import squire
+
+#adds a new element to the ui_elements dictionary in the squire class
+#the element passed in should either be the resource id of the element
+#or the xpath AS A STRING.  The name is whatever the user wants to call 
+#that element's key in the dictionary
+def add_element(self, element, name):
+    new_element = ""
+
+    if element[:2] == "//":
+        new_element = self.driver.find_element_by_xpath(element) 
+    else:
+        new_element = self.driver.find_element_by_id(element)
+
+    self._ui_elements[name] = new_element
+
+def get_element(self, name):
+    return self._ui_elements[name]
+
+
+
+
+
+'''
+
+def one_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/digit_1")
+    
+def two_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/digit_2")
+    
+def three_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/digit_3")
+    
+def four_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/digit_4")
+    
+def five_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/digit_5")
+    
+def six_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/digit_6")
+    
+def seven_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/digit_7")
+    
+def eight_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/digit_8")
+    
+def nine_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/digit_9")
+   
+def zero_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/digit_0")
+
+def decimal_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/dec_point")
+
+#OPERATORS
+
+def mult_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/op_mul")
+
+def div_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/op_div")
+
+def add_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/op_add")
+
+def subtract_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/op_sub")
+
+def delete_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/del")
+
+def clear_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/clr")
+
+def equals_button(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/eq")
+
+#SCREEN
+
+def display_screen(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/formula")
+
+def sub_result(self):
+    return self.driver.find_element_by_id("com.dalviksoft.calculator:id/result")
+
+
+'''
+
