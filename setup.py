738d899b1b10c35a64ea64dcfe2af5c0f77382f7
Joe Campbell <jcamp1095@gmail.com>
2016-08-19 10:51:06 -0400

    started prepping for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'squire',
+  packages = ['squire'], 
+  version = '0.1',
+  description = 'A platform for running tests on multiple Android devices in series',
+  author = 'John James Gallagher & Joseph Thomas Campbell',
+  author_email = 'jcamp1095@gmail.com',
+  url = 'https://github.com/JohnyGall/Squire', 
+  download_url = 'https://github.com/JohnyGall/Squire/tarball/0.1', 
+  keywords = ['testing', 'android', 'automation', 'appium', 'multiple', 'series'], 
+  classifiers = [],
+)
